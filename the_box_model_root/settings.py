79bb34857f6cda27d38ce9dfe6b5fca757e084ba
Viphakone Monty <10835390@my.uvu.edu>
2022-12-18 18:43:08 -0700

    deployment

--- the_box_model_root/settings.py
+++ the_box_model_root/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$_w_f9okivi8)ge9+u1f15f^&ec@=mq_6%@zvjhw$yea58)(12'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://the-box-1.herokuapp.com/', '127.0.0.1', '*']
 
